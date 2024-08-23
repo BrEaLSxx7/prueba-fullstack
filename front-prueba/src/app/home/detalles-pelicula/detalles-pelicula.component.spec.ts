7548a8e797f46c0ec4052b418360677f3e8582a0
Sebastian Cano <sebascano65@gmailcom>
2024-08-23 11:19:19 -0500

    Se termina pruebas unitarias

--- front-prueba/src/app/home/detalles-pelicula/detalles-pelicula.component.spec.ts
+++ front-prueba/src/app/home/detalles-pelicula/detalles-pelicula.component.spec.ts
@@ -1,17 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DetallesPeliculaComponent } from './detalles-pelicula.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HomeServiceService } from '../services/home-service.service';
+import { of, throwError } from 'rxjs';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { TimeConverterPipe } from '../../pipes/time-converter.pipe';
+import { DecimalPipe, DatePipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 describe('DetallesPeliculaComponent', () => {
   let component: DetallesPeliculaComponent;
   let fixture: ComponentFixture<DetallesPeliculaComponent>;
+  let mockHomeService: jasmine.SpyObj<HomeServiceService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DetallesPeliculaComponent]
-    })
-    .compileComponents();
+    mockHomeService = jasmine.createSpyObj('HomeServiceService', ['getComentarios']);
 
+    await TestBed.configureTestingModule({
+      imports: [
+        MatDialogModule,
+        MatButtonModule,
+        TimeConverterPipe,
+        DecimalPipe,
+        DatePipe,
+        MatCardModule,
+        MatProgressSpinnerModule
+      ],
+      declarations: [DetallesPeliculaComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: { _id: '1', title: 'Movie Title' } },
+        { provide: HomeServiceService, useValue: mockHomeService }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(DetallesPeliculaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +44,21 @@ describe('DetallesPeliculaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load comments on init', () => {
+    const mockComments = [{ text: 'Great movie!', date: new Date().toISOString() }];
+    mockHomeService.getComentarios.and.returnValue(of(mockComments));
+    component.ngOnInit();
+    expect(mockHomeService.getComentarios).toHaveBeenCalledWith('1');
+    expect(component.comentarios).toEqual(mockComments);
+    expect(component.isLoading).toBeFalse();
+  });
+
+  it('should handle error when loading comments', () => {
+    mockHomeService.getComentarios.and.returnValue(throwError('Error'));
+    component.ngOnInit();
+    expect(component.isLoading).toBeFalse();
+    expect(component.comentarios.length).toBe(0);
   });
 });
